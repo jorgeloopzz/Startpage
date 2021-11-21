189588794d4f146619886bd5e67dc30b4e46892d
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-21 20:05:12 +0000

    Set date

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 // Set date
 document.addEventListener('DOMContentLoaded', () => {
-    // Function to set time
     const timeTxt = document.getElementById('time');
+    const dateTxt = document.getElementById('date');
 
+    // Function to set time
     setInterval( () => {
         let date = new Date();
         let hours = date.getHours();
         let minutes = date.getMinutes();
         let seconds = date.getSeconds();
-        txt.innerHTML = hours + ':' + minutes + ':' + seconds;
+        timeTxt.innerHTML = hours + ':' + minutes + ':' + seconds;
     },1)
+
+    // Set date
+    const date = new Date();
+    
+    // Make to arrays to set days and months in text
+    const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    dateTxt.innerHTML = days[ date.getDay() ] + ' ' + date.getDate() + ' ' + months[ date.getMonth() ] + ' ' + date.getFullYear();
+
 });
