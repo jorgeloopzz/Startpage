c8bfb5a0d4c3a1b52b6dd0b410f608038ff37f8f
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-22 19:39:17 +0000

    Made months array and days array in the same array

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 // Set date
-document.addEventListener('DOMContentLoaded', () => {
-    const timeTxt = document.getElementById('time');
-    const dateTxt = document.getElementById('date');
+document.addEventListener("DOMContentLoaded", () => {
+  const timeTxt = document.getElementById("time");
+  const dateTxt = document.getElementById("date");
 
-    // Function to set time
-    setInterval( () => {
-        let date = new Date();
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
-        let seconds = date.getSeconds();
-        timeTxt.innerHTML = hours + ':' + minutes + ':' + seconds;
-    },1)
+  // Function to set time
+  setInterval(() => {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    timeTxt.innerHTML = hours + ":" + minutes + ":" + seconds;
+  }, 1);
 
-    // Set date
-    const date = new Date();
-    
-    // Make to arrays to set days and months in text
-    const days = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-    ];
+  // Set date
+  const date = new Date();
 
-    const months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ];
+  // Make arrays to set days and months in text
+  const daysMonths = [
+    // Days
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    // Months
+    [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+  ];
 
-    dateTxt.innerHTML = days[ date.getDay() ] + ' ' + date.getDate() + ' ' + months[ date.getMonth() ] + ' ' + date.getFullYear();
-
+  dateTxt.innerHTML =
+    daysMonths[date.getDay()] +
+    " " +
+    date.getDate() +
+    " " +
+    daysMonths[7][date.getMonth()] +
+    " " +
+    date.getFullYear();
 });
